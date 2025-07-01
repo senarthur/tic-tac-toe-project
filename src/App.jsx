76a36c2663ccf78a55921d9e29f8f48bfca158eb
Arthur Sena <sena.arthur698@gmail.com>
2025-07-01 10:56:47 -0300

    Feature: Log Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import Player from "./components/Player"
-import GameBoard from "./components/GameBoard"
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectedSquare() {
+  function handleSelectedSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => {
       return curActivePlayer === "X" ? "O" : "X";
     });
+
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+
+      if(prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+
+      return updatedTurns;
+    });
   }
-  
+
   return (
     <main>
       <div id="game-container">
@@ -19,10 +36,14 @@ function App() {
           <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectedSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard
+          onSelectSquare={handleSelectedSquare}
+          turns={gameTurns}
+        />
       </div>
+      <Log turns={gameTurns} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
