a3678dbe02d12b8a2eb430e9c9a951e8475ea054
Arthur Sena <sena.arthur698@gmail.com>
2025-07-01 09:25:40 -0300

    Feature: Game Board & Setting Turns

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
+
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectedSquare() {
+    setActivePlayer((curActivePlayer) => {
+      return curActivePlayer === "X" ? "O" : "X";
+    });
+  }
+  
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
+        <GameBoard onSelectSquare={handleSelectedSquare} activePlayerSymbol={activePlayer} />
       </div>
     </main>
   )
